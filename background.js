64e4de5dc5572b7aa6cceecb1db164450d8fb5b3
THANH TUNG <luuthanhtung04112001@gmail.com>
2021-12-22 04:20:56 +0700

    Update background.js

--- background.js
+++ background.js
@@ -3,17 +3,11 @@ try
 	console.clear();
 
 	chrome.contextMenus.create({title: "Function: Click to download image",id:"contextMenu"})
-	chrome.contextMenus.onClicked.addListener(function (info, tab) {
-		console.log("The API chrome.downloads will run as soon as you read this.");
-		chrome.downloads.download({url: "https://www.oxfordlearnersdictionaries.com/media/english/fullsize/c/coa/coast/coast.png"}, function (downloadId) {
-			console.log(`downloadId: ${downloadId}`)
-		})
-		chrome.downloads.onCreated.addListener(function (downloadItem) {
-			console.log(`state: ${downloadItem.state}`);
-			console.log(`startTime: ${downloadItem.startTime}`)
-			console.log(`endTime: ${downloadItem.endTime}`)
-		})
-	})
+
+	chrome.contextMenus.onClicked.addListener(async function (info, tab) {
+		const downloadId = await chrome.downloads.download({url: "https://www.oxfordlearnersdictionaries.com/media/english/fullsize/c/coa/coast/coast.png"});
+		console.log('downloadId:', downloadId);
+	});
 }
 catch (err)
 {
